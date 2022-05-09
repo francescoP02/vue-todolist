2cab1eeec27122a0af8784ca82d4947ca2bc2282
francescoP02 <file.partipilo@gmail.com>
2022-05-09 15:35:27 +0200

    Added MILESTONE1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
-const app = new VTTCue(
+const app = new Vue(
     {
         el : `#root`,
         data: {
-
+            todos: [
+                {
+                    text: 'Fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'Lavare i piatti',
+                    done: false
+                },
+                {
+                    text: 'Fare gli esercizi',
+                    done: false
+                },
+                {
+                    text: 'Preparare il pranzo',
+                    done: true
+                },
+            ],
         },
         methods: {
-            
+
         }
     }
 )
